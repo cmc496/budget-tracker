ae3abc32fbd3ef7cad34c7d29e9d47daaf9b685d
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-07 20:01:22 -0400

    debug3

--- public/service-worker.js
+++ public/service-worker.js
@@ -29,7 +29,7 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        cahces.keys().then(function (keyList) {
+        caches.keys().then(function (keyList) {
             let cacheKeepList = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
             });
